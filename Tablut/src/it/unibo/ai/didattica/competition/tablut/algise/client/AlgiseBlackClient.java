ff6e2ec650ceceda1670178f612751a0b18bd0ef
DavideFiloni <filonidavide@gmail.com>
2022-04-11 09:39:32 +0200

    Black e White Client

---
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/algise/client/AlgiseBlackClient.java
@@ -0,0 +1,18 @@
+package it.unibo.ai.didattica.competition.tablut.algise.client;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+
+public class AlgiseBlackClient {
+	
+	public static void main(String[] args) throws UnknownHostException, ClassNotFoundException, IOException {
+		String[] array = new String[]{"BLACK"};
+		if (args.length>0){
+			array = new String[]{"BLACK", args[0]};
+		}
+		AlgiseClient.main(array);
+	}
+
+
+}
---
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/algise/client/AlgiseWhiteClient.java
@@ -0,0 +1,18 @@
+package it.unibo.ai.didattica.competition.tablut.algise.client;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+
+public class AlgiseWhiteClient {
+	
+	public static void main(String[] args) throws UnknownHostException, ClassNotFoundException, IOException {
+		String[] array = new String[]{"WHITE"};
+		if (args.length>0){
+			array = new String[]{"WHITE", args[0]};
+		}
+		AlgiseClient.main(array);
+	}
+
+
+}
