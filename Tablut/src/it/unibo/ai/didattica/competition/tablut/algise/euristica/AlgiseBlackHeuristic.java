d0af72546580b796e74cb83d13d04d7ce1b90831
Filippo Comastri <filippo.comastri2@studio.unibo.it>
2022-04-16 10:48:27 +0200

    aggiornata BlackHeuristic

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/algise/euristica/AlgiseBlackHeuristic.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/algise/euristica/AlgiseBlackHeuristic.java
@@ -7,7 +7,7 @@ import it.unibo.ai.didattica.competition.tablut.algise.domain.Coordinate;
 import it.unibo.ai.didattica.competition.tablut.domain.State;
 
 public class AlgiseBlackHeuristic {
-	
+
 	private State state;
 	private int initB; // pedine NERE iniziali
 	private int initW; // pedine BIANCHE iniziali
@@ -18,110 +18,113 @@ public class AlgiseBlackHeuristic {
 	private Coordinate kingCoordinate;
 	private int blackNearKing; // pedine NERE vicine al RE
 	private int whiteNearKing; // pedine BIANCHE vicine al RE
-	
+
 	// Parametri da calcolare
 	private int pawnsB; // pedine NERE attuali
 	private int pawnsW; // pedine BIANCHE attuali
-	private int kingToCapture; //è possibile catturare il RE in una mossa? oppure essere accerchiato
-	private int pesiQuadranti[] = new int[4];
+	//private int kingToCapture; //è possibile catturare il RE in una mossa? oppure essere accerchiato
+	//private int pesiQuadranti[] = new int[4];
 	/*
 	 * private int whiteToCapture; //pedine BIANCHE catturabili
 	 * private int blackWarning; //pedine NERE a rischio cattura
 	 */
-	
-	private double WHITE_WEIGHT = 1.0;
-	private double BLACK_WEIGHT = 1.0;
-	private double KING_WEIGHT = 10.0;
-	/*
-	 * Estremi dei quadranti
-	 */
-	//Quadrante 1, quello alto a sinistra
-	private int QUAD_1=0; //Indice nel quadrante 1 nell'array
-	private int START_ROW_QUAD_1=0;
-	private int END_ROW_QUAD_1=3;
-	private int START_COL_QUAD_1=0;
-	private int END_COL_QUAD_1=0;
-	
-	//Quadrante 2, quello alto a destra
-	private int QUAD_2=1; //Indice nel quadrante 2 nell'array
-	private int START_ROW_QUAD_2=0;
-	private int END_ROW_QUAD_2=3;
-	private int START_COL_QUAD_2=5;
-	private int END_COL_QUAD_2=8;
-	
-	//Quadrante 3, quello basso a sinistra
-	private int QUAD_3=2; //Indice nel quadrante 3 nell'array
-	private int START_ROW_QUAD_3=5;
-	private int END_ROW_QUAD_3=8;
-	private int START_COL_QUAD_3=0;
-	private int END_COL_QUAD_3=0;
-	
-	//Quadrante 4, quello basso a destra
-	private int QUAD_4=3; //Indice nel quadrante 4 nell'array
-	private int START_ROW_QUAD_4=5;
-	private int END_ROW_QUAD_4=8;
-	private int START_COL_QUAD_4=5;
-	private int END_COL_QUAD_4=8;
+
+	//	/*
+	//	 * Estremi dei quadranti
+	//	 */
+	//	//Quadrante 1, quello alto a sinistra
+	//	private int QUAD_1=0; //Indice nel quadrante 1 nell'array
+	//	private int START_ROW_QUAD_1=0;
+	//	private int END_ROW_QUAD_1=3;
+	//	private int START_COL_QUAD_1=0;
+	//	private int END_COL_QUAD_1=0;
+	//	
+	//	//Quadrante 2, quello alto a destra
+	//	private int QUAD_2=1; //Indice nel quadrante 2 nell'array
+	//	private int START_ROW_QUAD_2=0;
+	//	private int END_ROW_QUAD_2=3;
+	//	private int START_COL_QUAD_2=5;
+	//	private int END_COL_QUAD_2=8;
+	//	
+	//	//Quadrante 3, quello basso a sinistra
+	//	private int QUAD_3=2; //Indice nel quadrante 3 nell'array
+	//	private int START_ROW_QUAD_3=5;
+	//	private int END_ROW_QUAD_3=8;
+	//	private int START_COL_QUAD_3=0;
+	//	private int END_COL_QUAD_3=0;
+	//	
+	//	//Quadrante 4, quello basso a destra
+	//	private int QUAD_4=3; //Indice nel quadrante 4 nell'array
+	//	private int START_ROW_QUAD_4=5;
+	//	private int END_ROW_QUAD_4=8;
+	//	private int START_COL_QUAD_4=5;
+	//	private int END_COL_QUAD_4=8;
 
 	//Pesi
-	private double REMAINING_BLACK_WEIGHT = 5.0;
-	private double REMAINING_WHITE_WEIGHT = 7.0;
-	private double WHITE_TO_CAPTURE_WEIGHT = 10.0;
-	private double BLACK_WARNING_WEIGHT = 8.0;
-	private double KING_TO_CAPTURE = 100.0;
+	private double BLACK_WEIGHT = 5.0;
+	private double WHITE_WEIGHT = 7.0;
+	//private double WHITE_TO_CAPTURE_WEIGHT = 10.0;
+	//private double BLACK_WARNING_WEIGHT = 8.0;
+	//private double KING_TO_CAPTURE = 100.0;
 
-	
-	private void initializeFields() {
+
+	private void extractFields() {
 		this.countPawns();
 		this.countPawnsNearKing();
-		this.setKingToCapture();
-		this.countWhiteToCapture();
-		this.setPesiQuadranti();
-	}
-	
-	private void calcolaPesoQuadrante(int startRow, int endRow, int startCol, int endCol, int quadrante)
-	{
-		/*
-		 * Dato che dobbiamo difendere una zona, più è popolata di bianchi più sarà alto il peso di questa.
-		 * In particolare sarà il RE che avendo un peso maggiore di tutti sbilancerà la situazione
-		 * Per i bianchi SOMMO, per i neri SOTTRAGGO
-		 */
-		//Quadrante 1
-		for(int i=startRow; i<endRow; i++)
-		{
-			for(int j=startCol; j<endCol; j++)
-			{
-				if(this.state.getPawn(i, j).equals(State.Pawn.WHITE))
-				{
-					this.pesiQuadranti[quadrante]+=WHITE_WEIGHT;
-				}
-				else if(this.state.getPawn(i, j).equals(State.Pawn.BLACK))
-				{
-					this.pesiQuadranti[quadrante]-=BLACK_WEIGHT;
-				}
-				else if(this.state.getPawn(i, j).equals(State.Pawn.KING))
-				{
-					this.pesiQuadranti[quadrante]+=KING_WEIGHT;
-				}
-				
-			}
-		}
+		//this.setKingToCapture();
+		//this.countWhiteToCapture();
+		//this.setPesiQuadranti();
 	}
 
-	private void setPesiQuadranti()
-	{
-		//Quadrante 1
-		this.calcolaPesoQuadrante(START_ROW_QUAD_1, END_ROW_QUAD_1, START_COL_QUAD_1, END_COL_QUAD_1, QUAD_1);
-		
-		//Quadrante 2
-		this.calcolaPesoQuadrante(START_ROW_QUAD_2, END_ROW_QUAD_2, START_COL_QUAD_2, END_COL_QUAD_2, QUAD_2);
-		
-		//Quadrante 3
-		this.calcolaPesoQuadrante(START_ROW_QUAD_3, END_ROW_QUAD_3, START_COL_QUAD_3, END_COL_QUAD_3, QUAD_3);
-		
-		//Quadrante 4
-		this.calcolaPesoQuadrante(START_ROW_QUAD_4, END_ROW_QUAD_4, START_COL_QUAD_4, END_COL_QUAD_4, QUAD_4);
-	}
+	//TODO metodo che assegna un peso maggiore in base alla distanza delle pedine dal re
+
+	//	private void calcolaPesoQuadrante(int startRow, int endRow, int startCol, int endCol, int quadrante)
+	//	{
+	//		/*
+	//		 * Dato che dobbiamo difendere una zona, più è popolata di bianchi più sarà alto il peso di questa.
+	//		 * In particolare sarà il RE che avendo un peso maggiore di tutti sbilancerà la situazione
+	//		 * Per i bianchi SOMMO, per i neri SOTTRAGGO
+	//		 */
+	//		//Quadrante 1
+	//		for(int i=startRow; i<endRow; i++)
+	//		{
+	//			for(int j=startCol; j<endCol; j++)
+	//			{
+	//				if(this.state.getPawn(i, j).equals(State.Pawn.WHITE))
+	//				{
+	//					this.pesiQuadranti[quadrante]+=WHITE_WEIGHT;
+	//				}
+	//				else if(this.state.getPawn(i, j).equals(State.Pawn.BLACK))
+	//				{
+	//					this.pesiQuadranti[quadrante]-=BLACK_WEIGHT;
+	//				}
+	//				else if(this.state.getPawn(i, j).equals(State.Pawn.KING))
+	//				{
+	//					this.pesiQuadranti[quadrante]+=KING_WEIGHT;
+	//				}
+	//				
+	//			}
+	//		}
+	//	}
+	//
+	//	private void setPesiQuadranti()
+	//	{
+	//		//Quadrante 1
+	//		this.calcolaPesoQuadrante(START_ROW_QUAD_1, END_ROW_QUAD_1, START_COL_QUAD_1, END_COL_QUAD_1, QUAD_1);
+	//		
+	//		//Quadrante 2
+	//		this.calcolaPesoQuadrante(START_ROW_QUAD_2, END_ROW_QUAD_2, START_COL_QUAD_2, END_COL_QUAD_2, QUAD_2);
+	//		
+	//		//Quadrante 3
+	//		this.calcolaPesoQuadrante(START_ROW_QUAD_3, END_ROW_QUAD_3, START_COL_QUAD_3, END_COL_QUAD_3, QUAD_3);
+	//		
+	//		//Quadrante 4
+	//		this.calcolaPesoQuadrante(START_ROW_QUAD_4, END_ROW_QUAD_4, START_COL_QUAD_4, END_COL_QUAD_4, QUAD_4);
+	//	}
+	//	
+	//	private void getPesoQuadranteRe() {
+	//		
+	//	}
 
 
 
@@ -142,7 +145,7 @@ public class AlgiseBlackHeuristic {
 			}
 		}
 	}
-	
+
 	// Calcolo pedine vicino al RE
 	private void countPawnsNearKing () {
 		int x = kingCoordinate.getX();
@@ -164,111 +167,111 @@ public class AlgiseBlackHeuristic {
 			else if (this.state.getPawn(x, y+1).equalsPawn(State.Pawn.WHITE.toString())) whiteNearKing++;
 		}
 	}
-	
-	
-	private void countWhiteToCapture () {
-		
-	}
-	
-	/**
-	 * Controlla se il re inizia ad essere accerchiato
-	 */
-	private void setKingToCapture() {
-		if(this.kingCoordinate!=null)
-		{
-			String kingBox = this.state.getBox(this.kingCoordinate.getX(), this.kingCoordinate.getY());
-			//Re sul trono, è accerchiato su 3 lati
-			if(kingBox.equals(State.Pawn.THRONE))
-			{
-				
-			}
-			//Re vicino ad un accampamento
-			else if(this.campsNearAttack(this.kingCoordinate.getX(), this.kingCoordinate.getY(), this.state))
-			{
-				
-			}
-			//Re di fianco al castello accerchiato su 2 lati
-			/*
-			else if()
-			{
-				
-			}
-			*/
-		}
-	}
-	
-	/**
-	 * Questo metodo controlla se il re è vicino al trono ed è circondato da due lati da una pedina nemica e il 
-	 * terzo è libero.
-	 * Esempio: 	T
-	 * 			   BKB
-	 * 				O
-	 * 
-	 * oppure		T
-	 * 			   OKB
-	 * 				B
-	 * 
-	 * oppure		T
-	 * 			   BKO
-	 * 				B
-	 * Nota: questi 3 casi di accerchiamento sono ad esempio solo per il caso del Trono sopra al RE, ma vanno ripetuti
-	 * anche per le casitiche di RE sopra al trono(riga -1), a destra del trono e a sinistra del trono.
-	 * @param row
-	 * @param column
-	 * @param state
-	 * @return
-	 */
-	private boolean throneNearAttack(int row, int column, State state) {
-		if(row > 0 && state.getPawn(row-1,column).equalsPawn(State.Pawn.THRONE.toString()) && 
-				row < state.getBoard().length-1 && state.getPawn(row+1,column).equalsPawn(State.Pawn.EMPTY.toString()) )
-			return true;
-		if( row < state.getBoard().length-1 && state.getPawn(row+1,column).equalsPawn(State.Pawn.THRONE.toString()) && 
-				row > 0  && state.getPawn(row-1,column).equalsPawn(State.Pawn.EMPTY.toString()) )
-			return true;
-		if(column > 0 && state.getPawn(row,column-1).equalsPawn(State.Pawn.THRONE.toString()) && 
-				column < state.getBoard().length-1 && state.getPawn(row,column+1).equalsPawn(State.Pawn.EMPTY.toString()) )
-			return true;
-		if( column < state.getBoard().length-1 && state.getPawn(row,column+1).equalsPawn(State.Pawn.THRONE.toString()) && 
-				column > 0  && state.getPawn(row,column-1).equalsPawn(State.Pawn.EMPTY.toString()) )
-			return true;
-		
-		return false;
-	}
-	
-	/**
-	 * Questo metodo controlla se il re è vicino ad un accampamento e il lato opposto è libero. La situazione
-	 * può essere vantaggiosa per il nero
-	 * @param row
-	 * @param column
-	 * @param state
-	 * @return
-	 */
-	private boolean campsNearAttack(int row, int column, State state) {
-		boolean blackInCamp = camps.contains(state.getBox(row, column)); //TODO chiedere a filo
-		if (blackInCamp) return false;
-		else {
-			if(row > 0 && camps.contains(state.getBox(row-1, column)) && 
-					row < state.getBoard().length-1 && state.getPawn(row+1,column).equalsPawn(State.Pawn.EMPTY.toString()) )
-				return true;
-			if( row < state.getBoard().length-1 && camps.contains(state.getBox(row+1, column)) && 
-					row > 0  && state.getPawn(row-1,column).equalsPawn(State.Pawn.EMPTY.toString()) )
-				return true;
-			if(column > 0 && camps.contains(state.getBox(row, column-1)) && 
-					column < state.getBoard().length-1 && state.getPawn(row,column+1).equalsPawn(State.Pawn.EMPTY.toString()) )
-				return true;
-			if( column < state.getBoard().length-1 && camps.contains(state.getBox(row, column+1)) && 
-					column > 0  && state.getPawn(row,column-1).equalsPawn(State.Pawn.EMPTY.toString()) )
-				return true;
-						
-		}
-		return true;
-	}
-	
+
+
+	//	/*private void countWhiteToCapture () {
+	//		
+	//	}*
+	//	
+	//	/**
+	//	 * Controlla se il re inizia ad essere accerchiato
+	//	 */
+	//	private void setKingToCapture() {
+	//		if(this.kingCoordinate!=null)
+	//		{
+	//			String kingBox = this.state.getBox(this.kingCoordinate.getX(), this.kingCoordinate.getY());
+	//			//Re sul trono, è accerchiato su 3 lati
+	//			if(kingBox.equals(State.Pawn.THRONE))
+	//			{
+	//				
+	//			}
+	//			//Re vicino ad un accampamento
+	//			else if(this.campsNearAttack(this.kingCoordinate.getX(), this.kingCoordinate.getY(), this.state))
+	//			{
+	//				
+	//			}
+	//			//Re di fianco al castello accerchiato su 2 lati
+	//			/*
+	//			else if()
+	//			{
+	//				
+	//			}
+	//			*/
+	//		}
+	//	}
+
+	//	/**
+	//	 * Questo metodo controlla se il re è vicino al trono ed è circondato da due lati da una pedina nemica e il 
+	//	 * terzo è libero.
+	//	 * Esempio: 	T
+	//	 * 			   BKB
+	//	 * 				O
+	//	 * 
+	//	 * oppure		T
+	//	 * 			   OKB
+	//	 * 				B
+	//	 * 
+	//	 * oppure		T
+	//	 * 			   BKO
+	//	 * 				B
+	//	 * Nota: questi 3 casi di accerchiamento sono ad esempio solo per il caso del Trono sopra al RE, ma vanno ripetuti
+	//	 * anche per le casitiche di RE sopra al trono(riga -1), a destra del trono e a sinistra del trono.
+	//	 * @param row
+	//	 * @param column
+	//	 * @param state
+	//	 * @return
+	//	 */
+	//	private boolean throneNearAttack(int row, int column, State state) {
+	//		if(row > 0 && state.getPawn(row-1,column).equalsPawn(State.Pawn.THRONE.toString()) && 
+	//				row < state.getBoard().length-1 && state.getPawn(row+1,column).equalsPawn(State.Pawn.EMPTY.toString()) )
+	//			return true;
+	//		if( row < state.getBoard().length-1 && state.getPawn(row+1,column).equalsPawn(State.Pawn.THRONE.toString()) && 
+	//				row > 0  && state.getPawn(row-1,column).equalsPawn(State.Pawn.EMPTY.toString()) )
+	//			return true;
+	//		if(column > 0 && state.getPawn(row,column-1).equalsPawn(State.Pawn.THRONE.toString()) && 
+	//				column < state.getBoard().length-1 && state.getPawn(row,column+1).equalsPawn(State.Pawn.EMPTY.toString()) )
+	//			return true;
+	//		if( column < state.getBoard().length-1 && state.getPawn(row,column+1).equalsPawn(State.Pawn.THRONE.toString()) && 
+	//				column > 0  && state.getPawn(row,column-1).equalsPawn(State.Pawn.EMPTY.toString()) )
+	//			return true;
+	//		
+	//		return false;
+	//	}
+	//	
+	//	/**
+	//	 * Questo metodo controlla se il re è vicino ad un accampamento e il lato opposto è libero. La situazione
+	//	 * può essere vantaggiosa per il nero
+	//	 * @param row
+	//	 * @param column
+	//	 * @param state
+	//	 * @return
+	//	 */
+	//	private boolean campsNearAttack(int row, int column, State state) {
+	//		boolean blackInCamp = camps.contains(state.getBox(row, column)); //TODO chiedere a filo
+	//		if (blackInCamp) return false;
+	//		else {
+	//			if(row > 0 && camps.contains(state.getBox(row-1, column)) && 
+	//					row < state.getBoard().length-1 && state.getPawn(row+1,column).equalsPawn(State.Pawn.EMPTY.toString()) )
+	//				return true;
+	//			if( row < state.getBoard().length-1 && camps.contains(state.getBox(row+1, column)) && 
+	//					row > 0  && state.getPawn(row-1,column).equalsPawn(State.Pawn.EMPTY.toString()) )
+	//				return true;
+	//			if(column > 0 && camps.contains(state.getBox(row, column-1)) && 
+	//					column < state.getBoard().length-1 && state.getPawn(row,column+1).equalsPawn(State.Pawn.EMPTY.toString()) )
+	//				return true;
+	//			if( column < state.getBoard().length-1 && camps.contains(state.getBox(row, column+1)) && 
+	//					column > 0  && state.getPawn(row,column-1).equalsPawn(State.Pawn.EMPTY.toString()) )
+	//				return true;
+	//						
+	//		}
+	//		return true;
+	//	}
+
 	public AlgiseBlackHeuristic (State state) {
 		this.state = state;
 		this.initB = 16;
 		this.initW = 9; // 8 + KING
-		
+
 		this.camps = Arrays.asList("a4", "a5", "a6", "b5", "d1", "e1", "f1", "e2", "i4", "i5", "i6", "h5", "d9",
 				"e9", "f9", "e8");
 
@@ -281,15 +284,24 @@ public class AlgiseBlackHeuristic {
 	}
 
 	public double evaluateState() {
+
+		//Resetto
 		this.reset();
-		this.initializeFields();
+
+		//Calcolo dei vari parametri
+		this.extractFields();
+
 		double result = 0;
+		//Se kingCoordinate==null significa che il re è stato mangiato, quindi quella mossa ha priorita massima 
 		if(kingCoordinate==null)
 		{
 			return Double.MAX_VALUE;
 		}
-		result+=this.pawnsB*this.REMAINING_BLACK_WEIGHT;
-		result-=this.pawnsW*this.REMAINING_WHITE_WEIGHT;
+
+		result+=this.pawnsB*this.BLACK_WEIGHT;
+		result-=this.pawnsW*this.WHITE_WEIGHT;
+		result+=this.blackNearKing*this.BLACK_WEIGHT;
+
 		return result;
 	}
 
@@ -299,7 +311,7 @@ public class AlgiseBlackHeuristic {
 		this.pawnsW=0;
 		this.blackNearKing=0;
 		//this.blackWarning=0;
-		this.kingToCapture=0;
+		//this.kingToCapture=0;
 	}
 
 }
