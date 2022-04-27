db774380a1cfe69ac07434aa6e43c9f2d5894454
fabioscaglia <fabioscagliarini99@gmail.com>
2022-04-27 18:32:24 +0200

    sistemato script lancio

--- Tablut/Executables/runmyplayer.sh
+++ Tablut/Executables/runmyplayer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar /home/tablut/tablut/AlgiseTablut/Tablut/Executables/AlgiseBlackClient.jar "$1" "$2" "$3"
+java -jar /home/tablut/tablut/AlgiseTablut/Tablut/Executables/AlgiseClient.jar "$1" "$2" "$3"
